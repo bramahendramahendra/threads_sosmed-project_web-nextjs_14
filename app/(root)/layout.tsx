5f386612fd1c3f42b30defb717b10c2a6592da6f
bramahendramahendra <bramahendramahendra@gmail.com>
2023-12-04 20:10:47 +0700

    create components file

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,12 @@
-import type { Metadata } from 'next'
 import { ClerkProvider } from "@clerk/nextjs"
+import '../globals.css'
+import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-import '../globals.css'
+import Topbar from '@/components/shared/Topbar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/RightSidebar'
+import Bottombar from '@/components/shared/Bottombar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +23,23 @@ export default function RootLayout({
   return (
     <ClerkProvider>
         <html lang="en">
-          <body className={inter.className}>{children}</body>
+          <body className={inter.className}>
+            <Topbar />
+
+            <main>
+              <LeftSidebar />
+              
+              <section className='main-container'>
+                <div className='w-full max-x-4xl'>
+                  {children}
+                </div>
+              </section>
+
+              <RightSidebar />
+            </main>
+
+            <Bottombar />
+          </body>
         </html>
     </ClerkProvider>
   )
