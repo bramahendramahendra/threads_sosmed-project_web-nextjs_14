4bdafccfaea0b76f4fe0032e82419a750aa09911
bramahendramahendra <bramahendramahendra@gmail.com>
2023-12-12 17:07:02 +0700

    dev thread by ID in Post Menu Home

---
+++ app/(root)/thread/[id]/page.tsx
@@ -0,0 +1,37 @@
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchThreadById } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const Page = async ({ params }: { params: { id: string }}) => {
+    if(!params.id) return null;
+
+    const user = await currentUser();
+    if(!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+    if(!userInfo?.onboarderd) redirect('/onboarding')
+
+    const thread = await fetchThreadById(params.id);
+
+    return (
+            <section className="relative">
+                <div>
+                    <ThreadCard
+                        key={thread._id}
+                        id={thread._id}
+                        currentUserId={user?.id || ""}
+                        parrentId={thread.parrentId}
+                        content={thread.text}
+                        author={thread.author}
+                        community={thread.community}
+                        createdAt={thread.createdAt}
+                        comments={thread.children}
+                    />
+                </div>
+            </section>
+        )
+    }
+
+export default Page;
